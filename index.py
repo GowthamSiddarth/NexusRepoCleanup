04b23a47cb2f859ac51b685688d730a4c754dd86
Gowtham Behara <gothsiddu@gmail.com>
2018-07-26 20:26:38 +0530

    Validate args passed by cmnd

--- index.py
+++ index.py
@@ -1,19 +1,19 @@
-import argparse, logging
+import argparse, logging, re
 
 
-def init_logger():
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.DEBUG)
+def validate_args(args):
+    logger.info("Started executing validate_args()")
 
-    console_handler = logging.StreamHandler()
-    console_handler.setLevel(logging.DEBUG)
+    args['host'] = args['host'].strip('/')
+    regex = re.compile(
+        r'^(?:http|ftp)s?://'  # http:// or https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
+        r'localhost|'  # localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
+        r'(?::\d+)?'  # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
 
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-    console_handler.setFormatter(formatter)
-    logger.addHandler(console_handler)
-
-    return logger
+    return args['keep'].isdigit() and re.match(regex, args['host'])
 
 
 def parse_args():
@@ -32,9 +32,26 @@ def parse_args():
 
 def main(logger):
     logger.info("Started executing main function")
+
     args = parse_args()
     logger.debug("Arguments parsed are: " + str(args))
+
     logger.info("Main function execution finished.")
+
+
+def init_logger():
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+
+    return logger
 
 
 if __name__ == '__main__':
